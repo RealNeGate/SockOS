afa7796912217020af775ca28e416a2a684bc2c5
Yasser Arguelles Snape <yasserarguelles2506@gmail.com>
2025-11-03 17:52:11 -0800

    Removing spall from default run.bat (for now)

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
-qemu-system-x86_64 -bios OVMF.fd -drive format=raw,file=fat:rw:bin -no-reboot -d int -D qemu.log -m 1024M -smp cores=1,threads=1,sockets=1 -serial stdio -serial file:perf.spall
-dd if="perf.spall" of="perf2.spall" bs=1 skip=367
+qemu-system-x86_64 -bios OVMF.fd -drive format=raw,file=fat:rw:bin -no-reboot -d int -D qemu.log -m 1024M -smp cores=1,threads=1,sockets=1 -serial stdio
 
 
